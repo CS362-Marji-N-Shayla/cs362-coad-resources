dd127257c47fd0ae8b5363e17907cf2b63ec20f6
Marji Symonds <symondsm@oregonstate.edu>
2020-04-16 09:37:10 -0700

    Add to_s test for resource category

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe ResourceCategory, type: :model do
         ResourceCategory.new
     end
 
+    it 'has a string representation that is the name' do
+        r = Region.new(name: 'FAKE')
+        expect(r.to_s).to eq('FAKE')
+    end
+
 end
