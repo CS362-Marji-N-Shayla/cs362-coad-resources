d6d1c728a26eab484a71d072e74b621cc2f7664a
Marji Symonds <symondsm@oregonstate.edu>
2020-04-16 09:31:14 -0700

    Add to_s test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+	   it 'has a string representation that is the email' do
+        r = User.new(email: 'FAKE')
+        expect(r.to_s).to eq('FAKE')
+    end
+
 end
