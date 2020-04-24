a2b52f155a02ce4809da3788701d90898ffb8ca8
Marji Symonds <symondsm@oregonstate.edu>
2020-04-24 15:22:03 -0700

    Add validation tests for organization

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe ResourceCategory, type: :model do
     end
 
     describe "relationships" do
-        let(:resource_category) { RescourceCategory.new }
-        
+        let(:resource_category) { ResourceCategory.new }
+
         it "has and belongs to many organizations" do
             expect(resource_category).to have_and_belong_to_many(:organizations)
         end
