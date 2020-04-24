2750f7ee8308922acb257ad4d164ea7290e9940b
Shayla Lane <lanesha@oregonstate.edu>
2020-04-24 15:22:55 -0700

    Test validity of resource category

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
+    let(:resource_category) { RescourceCategory.new }
+
     it 'has a string representation that is the name' do
-        resource_category = ResourceCategory.new(name: 'FAKE')
-        expect(resource_category.to_s).to eq('FAKE')
+        expected = 'FAKE'
+        resource_cat = ResourceCategory.new(name: 'FAKE')
+        expect(resource_cat.to_s).to eq(name: expected)
     end
 
-    describe "relationships" do
-        let(:resource_category) { RescourceCategory.new }
-        
+    describe "relationships" do        
         it "has and belongs to many organizations" do
             expect(resource_category).to have_and_belong_to_many(:organizations)
         end
@@ -16,7 +17,18 @@ RSpec.describe ResourceCategory, type: :model do
         it "has many tickets" do
             expect(resource_category).to have_many(:tickets)
         end
-        
+    end
+
+    describe "validations" do
+        it "validates name" do
+            expect(resource_category).to validate_presence_of(:name)
+        end
+        it "validates length of name" do
+            expect(resource_category).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+        end
+        it "validates uniqueness of name" do
+            expect(resource_category).to validate_uniqueness_of(:name).case_insensitive
+        end
     end
 
 end
