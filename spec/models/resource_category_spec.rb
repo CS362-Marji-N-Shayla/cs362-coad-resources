9fe25058a15406471064bce286963a0e84df1c23
Marji Symonds <symondsm@oregonstate.edu>
2020-04-28 09:38:26 -0700

    Add test for active scope in ResourceCategory

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -9,6 +9,11 @@ RSpec.describe ResourceCategory, type: :model do
         expect(resource_cat.to_s).to eq(expected_name)
     end
 
+    it 'can determine if a record is inactive' do
+        active_resource_cat = ResourceCategory.new(active: true,name: 'FAKE')
+        expect(resource_cat.inactive?).to be_falsy
+    end
+
     describe "relationships" do
       it "has and belongs to many organizations" do
           expect(resource_category).to have_and_belong_to_many(:organizations)
@@ -31,5 +36,26 @@ RSpec.describe ResourceCategory, type: :model do
         end
     end
 
-end
+    describe "#active" do
+      it "active resource_categories" do
+        active_resource_category = ResourceCategory.create(active: true, name: 'FAKEACTIVE')
+        inactive_resource_category = ResourceCategory.create(active: false, name: 'FAKEINACTIVE')
 
+        active_resource_categories = ResourceCategory.active
+        expect(active_resource_categories).to include(active_resource_category)
+        expect(active_resource_categories).not_to include(inactive_resource_category)
+      end
+    end
+
+    describe "#inactive" do
+      it "inactive resource_categories" do
+        active_resource_category = ResourceCategory.create(active: true, name: 'FAKEACTIVE')
+        inactive_resource_category = ResourceCategory.create(active: false, name: 'FAKEINACTIVE')
+
+        inactive_resource_categories = ResourceCategory.inactive
+        expect(inactive_resource_categories).to include(inactive_resource_category)
+        expect(inactive_resource_categories).not_to include(active_resource_category)
+      end
+    end
+
+end
