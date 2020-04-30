5d45a2b79864cf2096be7b36f71fac09ce4a7497
Shayla Lane <lanesha@oregonstate.edu>
2020-04-30 09:29:21 -0700

    Stub out regions factory bot

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Region, type: :model do
-    let(:region) { Region.new }
+	let(:region) { build(:region) }
+	
     it 'has a string representation that is the name' do
         region = Region.new(name: 'FAKE')
         expect(region.to_s).to eq('FAKE')
