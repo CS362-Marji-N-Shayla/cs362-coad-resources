338f0a8567c22002641ca394a8d5eeed22758977
Marji Symonds <symondsm@oregonstate.edu>
2020-05-08 15:44:53 -0700

    Go back to safety

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Ticket, type: :model do
   let(:ticket) { FactoryBot.build(:ticket) }
   let(:region) { FactoryBot.create(:region)}
   let(:resource_category) { FactoryBot.create(:resource_category)}
-  let(:organization) { FactoryBot.build(:organization) }
+  let(:organization) { FactoryBot.create(:organization) }
 
 
   it 'has a string representation that is the name' do
@@ -82,14 +82,6 @@ RSpec.describe Ticket, type: :model do
       ticket.closed = true
       expect(ticket).to be_closed
       expect(ticket).not_to be_open
-    end
-  end
-
-  describe '#organization' do
-    it 'is all_organization scope' do
-      ticket.closed = false
-      ticket.organization = organization.organization_id
-      expect(ticket).to be_all_organization
     end
   end
 
