8debb6366a73ab75939afd590a3e81333d75c422
Shayla Lane <lanesha@oregonstate.edu>
2020-05-22 09:45:33 -0700

    Test admin deletes region

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
+    before :each do
+        @region = FactoryBot.create(:region)
+        @user = FactoryBot.create(:user, :admin)
+    end
 
+    before do
+        log_in_as(@user)
+    end
+
+    context 'as an admin' do
+        it 'deletes a region' do
+            visit region_path(@region)
+            byebug
+            click_on 'Delete'
+            expect(page).to have_content('Regions')
+            expect(page).to have_content('was deleted')
+        end
+    end
 end
--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Resource Category', type: :feature do
+    before :each do
+        @resource_category = FactoryBot.create(:resource_category)
+        @user = FactoryBot.create(:user, :admin)
+    end
 
+    it 'displays a success message' do
+        visit "/resource_categories/#{@resource_category.id}"
+        click_link 'Delete'
+        expect(page).to have_content('Category Fake was deleted')
+    end
 end
+
+
+# Category Fake was deleted
