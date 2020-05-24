61443495ab32e3c74164d5e85971ce901438fbac
Marji Symonds <symondsm@oregonstate.edu>
2020-05-23 17:33:16 -0700

    Start organization app feature test(wip) and add organization controller test(wip)

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -2,4 +2,40 @@ require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
 
+	class FakeMailer
+		def new_organization_application
+			false
+		end
+	end	
+
+	context 'Organization user' do
+		describe 'POST #create' do
+			let(:user) do
+				User.create(email: 'fake@fake.com', password: 'password',
+					password_confirmation: 'password', role: 'organization')
+			end
+			before do
+				user.confirm
+				sign_in(user)
+			end
+			it 'succeeds' do
+				allow(UserMailer).to receive(:with).and_return(FakeMailer.new)
+				expect(
+					post(
+						:create,
+						params: {
+							organization: {name: 'Fake',
+								email: 'fake@fake.com',
+								phone: '13036531234',
+								primary_name: 'Fake_primary',
+								secondary_name: 'Fake_secondary',
+								secondary_phone: '13036534332'
+							},
+						}
+					)
+				).to redirect_to(organization_application_submitted_path)
+			end
+		end
+	end
+
 end
--- spec/features/organizations/create_organization_application_spec.rb
+++ spec/features/organizations/create_organization_application_spec.rb
@@ -2,4 +2,33 @@ require 'rails_helper'
 
 RSpec.describe 'Creating an Organization Application', type: :feature do
 
+	# before(:each) do
+	# 	user = FactoryBot.create(:user)
+	# 	log_in_as(user)
+	# end
+
+	it 'displays a success message' do
+		visit new_organization_path
+		choose 'organization_liability_insurance_true'
+		choose 'organization_agreement_one_true'
+		choose 'organization_agreement_two_true'
+		choose 'organization_agreement_three_true'
+		choose 'organization_agreement_four_true'
+		choose 'organization_agreement_five_true'
+		choose 'organization_agreement_six_true'
+		choose 'organization_agreement_seven_true'
+		choose 'organization_agreement_eight_true'
+		fill_in 'What is your name? (Last, First)', with: 'Fake Name'
+		fill_in 'Organization Name', with: 'FakeOrg'
+		fill_in 'What is your direct phone number? Cell phone is best', with: '555-543-1234'
+		fill_in 'Who may we contact regarding your organization\'s application if we are unable to reach you?', with: "Fake Secondary"
+		fill_in 'What is a good secondary phone number we may reach your organization at', with: '541-123-3241'
+		fill_in 'organization_email', with: 'fake@fakeemail.com'
+		fill_in 'Description', with: 'Fake description'
+		choose 'organization_transportation_yes'
+		click_on 'Apply'
+		expect(page).to have_content('Application Submitted')
+	end
+
+
 end
