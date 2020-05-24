f9fa6d273b9dbcd3704da61b508f20c41b8821a1
Marji Symonds <symondsm@oregonstate.edu>
2020-05-24 13:46:30 -0700

    Add approve organization feature test

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
 
+	before(:each) do
+		organization = FactoryBot.create(:organization)
+		user = FactoryBot.create(:user, :admin)
+		log_in_as(user)
+	end
+
+	it 'moves an organization to approved when I approve it' do
+		visit organizations_url
+		click_on 'Pending'
+		click_on 'Review'
+		click_on 'Approve'
+		expect(page).to have_content('Approved')
+	end
+
 end
