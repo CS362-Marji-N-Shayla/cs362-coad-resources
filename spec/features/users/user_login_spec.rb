e044b9324813bcf172a87563693877e3ef13ed71
Marji Symonds <symondsm@oregonstate.edu>
2020-05-31 11:38:21 -0700

    Add user log in feature test

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe 'Logging in', type: :feature do
 
 	before(:each) do
-		user = FactoryBot.create(:user)
+		@user = FactoryBot.create(:user)
 	end
 
-	it 'moves an organization to approved when I approve it' do
-		visit login
-		fill_in 'Email address', with: user.email
-		fill_in 'Email address', with: user.password
+	it "logs user in when I fill in their credentials" do
+		visit login_url
+		fill_in 'Email address', with: @user.email
+		fill_in 'Password', with: 'FAKEPASSWORD'
 		click_on 'Sign in'
 		expect(page).to have_content('Dashboard')
 	end
