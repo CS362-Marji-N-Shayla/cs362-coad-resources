cf69ac5fab73e53f185b66f885ade55040ee00b0
Marji Symonds <symondsm@oregonstate.edu>
2020-05-31 13:45:09 -0700

    Add user registration feature test (wip)

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
 
+	it "creates a new user when I sign up" do
+		visit signup_url
+		fill_in 'Email address', with: 'fake99@fake.com'
+		fill_in 'Password', with: 'FAKEPASSWORD'
+		fill_in 'Password confirmation', with: 'FAKEPASSWORD'
+		# check 'I\'m not a robot'
+		click_button 'Sign up'
+		click_on 'Confirm my account'
+		expect(page).to have_content('successfully confirmed')
+	end
+
 end
