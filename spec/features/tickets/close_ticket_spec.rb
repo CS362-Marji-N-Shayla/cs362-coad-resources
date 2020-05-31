0c39480717a184ba58df45a142c2d207dee869c7
Marji Symonds <symondsm@oregonstate.edu>
2020-05-31 14:01:44 -0700

    Close ticket feature test

--- spec/features/tickets/close_ticket_spec.rb
+++ spec/features/tickets/close_ticket_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Closing a ticket', type: :feature do
+	before :each do
+        @ticket = FactoryBot.create(:ticket)
+        @user = FactoryBot.create(:user, :admin)
+        log_in_as(@user)
+    end
+
+    it 'displays a success message' do
+        visit ticket_path(@ticket)
+        click_link 'Close'
+        expect(page).to have_content('Ticket')
+        expect(page).to have_content('was closed')
+    end
 
 end
