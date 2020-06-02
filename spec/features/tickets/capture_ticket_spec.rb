46bc4ae58a1e13970365e69f552c9f3077f66f5c
Marji Symonds <symondsm@oregonstate.edu>
2020-06-01 20:30:14 -0700

    Add release ticket feature test

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe 'Capturing a ticket', type: :feature do
 	it "takes user to dashboard after capturing a ticket" do
       visit ticket_path(@ticket.id)
       click_on 'Capture'
-      # click_on @ticket.name
       expect(current_path).to eq(dashboard_path)
       expect(page).to have_content(@ticket.name)
     end
--- spec/features/tickets/release_ticket_spec.rb
+++ spec/features/tickets/release_ticket_spec.rb
@@ -2,16 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Releasing a ticket by an', type: :feature do
 	before :each do
-        @captured_ticket = FactoryBot.create(:ticket, :captured)
-        @user = FactoryBot.create(:user)
+        organization = FactoryBot.create(:organization, :approved)
+        @user = FactoryBot.create(:user, organization: organization)
+        @captured_ticket = FactoryBot.create(:ticket, organization: organization)
         log_in_as(@user)
     end
 	#release ticket from dashboard
     it 'releases a ticket' do
-        visit ticket_path(@captured_ticket)
+        visit ticket_path(@captured_ticket.id)
         click_link 'Release'
-        visit dashboard_url
-        select 'Released', from: 'status'
         expect(page).to have_content(@captured_ticket.name)
     end
 end
